162ce7ca507f8cbf9999e3fcefac4c6d95954039
pedroguto167 <101297527+pedroguto167@users.noreply.github.com>
2023-12-11 21:37:24 -0300

    Add files via upload

---
+++ feed/xmlrpc0db0.php
@@ -0,0 +1,15 @@
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+	<service>
+		<engineName>WordPress</engineName>
+		<engineLink>https://wordpress.org/</engineLink>
+		<homePageLink>appsinais.online/appteste01/bullshacker-vip/</homePageLink>
+		<apis>
+			<api name="WordPress" blogID="1" preferred="true" apiLink="appsinais.online/appteste01/bullshacker-vip//xmlrpc.php" />
+			<api name="Movable Type" blogID="1" preferred="false" apiLink="appsinais.online/appteste01/bullshacker-vip//xmlrpc.php" />
+			<api name="MetaWeblog" blogID="1" preferred="false" apiLink="appsinais.online/appteste01/bullshacker-vip//xmlrpc.php" />
+			<api name="Blogger" blogID="1" preferred="false" apiLink="appsinais.online/appteste01/bullshacker-vip//xmlrpc.php" />
+				<api name="WP-API" blogID="1" preferred="false" apiLink="appsinais.online/appteste01/bullshacker-vip//wp-json/" />
+			</apis>
+	</service>
+</rsd>
+	
